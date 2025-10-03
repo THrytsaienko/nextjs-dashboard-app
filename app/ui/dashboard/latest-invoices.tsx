4c45d67763eb690f03d944f19dc5fe46e3abb9a2
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2025-10-03 16:46:00 +0300

    Create, update, delete invoice

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,13 +2,13 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
 import { fetchLatestInvoices } from '@/app/lib/data';
-export default async function LatestInvoices({
-  // latestInvoices,
-}: {
-  // latestInvoices: LatestInvoice[];
-}) {
+// import { LatestInvoice } from '@/app/lib/definitions';
+
+// export default async function LatestInvoices({latestInvoices}: { latestInvoices: LatestInvoice[]; }) {
+//     const latestInvoices = await fetchLatestInvoices();
+
+export default async function LatestInvoices() {
     const latestInvoices = await fetchLatestInvoices();
 
     return (
